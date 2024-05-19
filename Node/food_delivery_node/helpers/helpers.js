f467394a1158b9bd74a6a2b7d19c6d7783d88905
Newtr <canaladidasq@yandex.ru>
2024-05-19 03:37:07 +0300

    video Build a Robust GOTOVO

--- Node/food_delivery_node/helpers/helpers.js
+++ Node/food_delivery_node/helpers/helpers.js
@@ -104,6 +104,17 @@ module.exports = {
         return result;
     },
 
+    createNumber: (length = 6) => {
+        var chars = "0123456789"
+        var result = '';
+        for (let i = length; i > 0; i--) {
+            result += chars[Math.floor(Math.random() * chars.length)];
+
+        }
+
+        return result;
+    },
+
     Dlog: (log) => {
         return Dlog(log);
     },
